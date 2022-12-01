97f9351eed90f9c392863dc3d3a011379f80ab3f
TaseerH <73301847+TaseerH@users.noreply.github.com>
2022-12-01 16:55:50 +0500

    Updates

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View, Image, Button, Modal,TextInput, Pressable, Dimensions  } from 'react-native'
+import { StyleSheet, Text, View, Image, Modal,TextInput, Pressable, Dimensions, } from 'react-native'
   import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
   import React from 'react'
-  import LinearGradient from 'react-native-linear-gradient'
+  import DatePicker from 'react-native-date-picker'
   import { FloatingAction } from "react-native-floating-action";
   import { useState } from "react";
   import DropDownPicker from 'react-native-dropdown-picker';
 
 const Dashboard = () => {
+
   const { width } = Dimensions.get("window");
   const actions = [
     {
@@ -27,110 +28,92 @@ const Dashboard = () => {
   const toggleModalVisibility = () => {
       setModalVisible(!isModalVisible);
   };
+
+  const [expense, setExpense] = useState([]);
+  const [date, setDate] = useState(new Date())
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState([]);
   const [items, setItems] = useState([
-    {label: 'Spain', value: 'spain'},
-    {label: 'Madrid', value: 'madrid', parent: 'spain'},
-    {label: 'Barcelona', value: 'barcelona', parent: 'spain'},
+    {label: 'Food & Drinks', value: 'Food & Drinks'},
+    {label: 'Shopping', value: 'Shopping',},
+    {label: 'Fuel', value: 'Fuel', parent: 'Transport'},
 
-    {label: 'Italy', value: 'italy'},
-    {label: 'Rome', value: 'rome', parent: 'italy'},
+    {label: 'Transport', value: 'Transport', },
+    {label: 'Restaurant', value: 'Restaurant', parent: 'Food & Drinks'},
+    {label: 'Groceries', value: 'Groceries', parent: 'Food & Drinks'},
 
-    {label: 'Finland', value: 'finland'},
-    {label: 'Pakistan', value: 'pakistan'},
-    {label: 'Islamabad', value: 'islamabad', parent: 'pakistan'}
+    {label: 'Clothes', value: 'Clothes', parent: 'Shopping'},
+    {label: 'Bills & Fees', value: 'Bills & Fees'},
   ]);
+  const real_date = date.toDateString()
   return (
     <KeyboardAwareScrollView
-      style={{ backgroundColor: '#4c69a5' }}
+      style={{ backgroundColor: 'black' }}
       resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={styles.container}
       scrollEnabled={true}>
       <View style={{ height: 950, width: 'auto'}}>
-      <LinearGradient start = {{x:0, y:0}} end={{x:1, y:1}} colors={['#1d0ccb', '#3f3dcb', '#af322c']} locations = {[0.03, 0.1, 0.88]}style = {{height: 1000, width: 'auto'}}>
-      <View style={{width: 'auto', height: 550, marginTop: 20, borderRadius: 35, backgroundColor: 'white' }}>
+      
+      <View style={{width: 'auto', height: 250, marginTop: 20, borderRadius: 35, backgroundColor: 'black' }}>
             <View style={{flexDirection:'row', alignItems:'center'}}>
             <Image source={{uri: 'https://o.remove.bg/downloads/13912b89-df34-4d65-830d-cce2b3b8b646/black-man-with-beard-avatar-character-free-vector-removebg-preview.png'}} style={{height: 100, width: 100, resizeMode: 'cover'}}></Image>
-            <Text style={{color: 'black' ,
+            <Text style={{color: 'white' ,
                 fontSize: 20, 
                 fontStyle: 'bold', }}>Taseer Haider</Text>
             </View>
-            
-            <View elevation={10} style={{width: 250, height: 350, alignSelf:'center', marginTop: 20,  backgroundColor: '#01DBB7', borderRadius: 15, shadowColor: '#000000',
-                    shadowOffset: {
-                    width: 10,
-                    height: 10
-                    },
-                    shadowRadius: 5,
-                    shadowOpacity: 1.0}}>
-                
-                    <Text style={{color: 'white' ,
-                        fontSize: 20, 
-                        marginTop: 20,
-                        fontStyle: 'italic', alignSelf:'center'}}>Taseer Haider</Text>
-                    <View style={{flexDirection:'row', alignItems:'baseline', marginTop: 20}}>
-                        <Image source={{uri: 'https://o.remove.bg/downloads/f7f18079-9efb-42c8-858e-0bd35cbd6658/png-transparent-mastercard-credit-card-visa-payment-service-mastercard-company-orange-logo-removebg-preview.png'}} style={{height: 100, width: 100, resizeMode:'cover'}}></Image>
+            <View style={{flexDirection:'row',}}>
+            <View style={{width: 180, height: 90, marginTop: 35, marginLeft: 30, backgroundColor: '#01DBB7', borderRadius: 15,}}>
+                   
+                    <Pressable style={{height: 90, width: 180, alignSelf: 'center', marginTop: 20 , borderRadius: 10 }}>
                         <Text style={{color: 'white' ,
-                        fontSize: 20, 
-                        marginTop: 20,
-                        fontStyle: 'italic', alignSelf:'center'}}>545454545454</Text>
-                    </View>
-                    <Text style={{color: 'white' ,
+                        fontSize: 20,
+                        fontStyle: 'bold', alignSelf:'center'}}>Income</Text>
+                          <Text style={{color: 'white' ,
                         fontSize: 20, 
                         marginTop: 5,
-                        fontStyle: 'italic', alignSelf:'center'}}>Exp Date: 10/2026</Text>
-                    <Text style={{color: 'white' ,
-                        fontSize: 20, 
-                        marginTop: 5,
-                        fontStyle: 'italic', alignSelf:'center'}}>CVC: 156</Text>
-                    <Pressable style={{height: 35, width: 60, backgroundColor: '#195249', alignSelf: 'center', marginTop: 80, borderRadius: 10 }}>
-                        <Text style={{color: 'white' ,
-                        fontSize: 20, 
-                        marginTop: 5,
-                        fontStyle: 'bold', alignSelf:'center'}}>Copy</Text>
+                        fontStyle: 'italic', alignSelf:'center'}}>Rs. 2500</Text>
                     </Pressable>
                 
             </View>
-            
+            <View style={{width: 180, height: 90, marginTop: 35, marginLeft: 30, backgroundColor: '#E1E1E1', borderRadius: 15,}}>
+                   
+                    <Pressable style={{height: 90, width: 180, alignSelf: 'center', marginTop: 20 , borderRadius: 10 }}>
+                        <Text style={{color: 'black' ,
+                        fontSize: 20,
+                        fontStyle: 'bold', alignSelf:'center'}}>Expenses</Text>
+                          <Text style={{color: 'black' ,
+                        fontSize: 20, 
+                        marginTop: 5,
+                        fontStyle: 'italic', alignSelf:'center'}}>Rs. 12</Text>
+                    </Pressable>
+                
+            </View>
+            </View> 
       </View>  
         
-        <View style={{width: 'auto', height: 700, marginTop: 20, borderRadius: 35, backgroundColor: 'white' }}>
-        <Image source={{uri: 'https://o.remove.bg/downloads/62fd7d49-1cc2-45d5-961d-0b6d2aa5415d/image-removebg-preview.png'}} style={{height: 30, width: 150, marginLeft: 150, resizeMode: 'cover',}}></Image>   
-        <TextInput
-          placeholder="Enter name" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
-          color: 'black' ,
-          fontSize: 20, 
-          fontStyle: 'bold', 
-          marginTop: 100, 
-          marginLeft: 30, 
-          padding: 8, 
-          width: 400,
-          textAlign: 'center', 
-          paddingTop: 10, 
-          borderColor: '#28558a', 
-          borderWidth: 2, 
-          borderRadius: 10, }}
-        />
-        <TextInput
-          placeholder="Enter email" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
-          fontSize: 20, 
-          fontStyle: 'bold', 
-          marginTop: 20, 
-          marginLeft: 30, 
-          padding: 8, 
-          width: 400,
-          textAlign: 'center', 
-          paddingTop: 10, 
-          borderColor: '#28558a', 
-          borderWidth: 2, 
-          borderRadius: 10, }}
-        />
+        <View style={{width: 'auto', height: 700, marginTop: 20, borderRadius: 35, backgroundColor: 'black' }}>
+        <Image source={{uri: 'https://o.remove.bg/downloads/3c80613c-2fc3-4fd5-858f-515d28eed0c2/Screenshot_2022-11-21_233640-removebg-preview.png'}} style={{height: 30, width: 350, alignSelf: 'center', resizeMode: 'cover',}}></Image>   
+        <Text style={{ textAlign: 'center', fontSize: 18, margin: 15}}>Expenses List</Text>
+        <View style={{width: 380, height: 90, marginTop: 35, alignSelf: 'center', backgroundColor: '#5D5D5D', borderRadius: 15,}}>
+                   
+                    <Pressable style={{height: 90, width: 320, alignSelf: 'center',  marginTop: 20 , }}>
+                        <View style={{flexDirection: 'row',}}>
+                        <Text style={{color: '#01DBB7' ,
+                        fontSize: 20,
+                        fontStyle: 'bold', textAlign: 'left', }}>{value}</Text>
+                        <Text style={{color: '#01DBB7' ,
+                        fontSize: 20, 
+                        marginTop: 5,
+                        fontStyle: 'italic', marginLeft: 80,}}>{real_date}</Text>
+                        </View>
+                        <Text style={{color: '#01DBB7' ,
+                        fontSize: 20, 
+                        marginTop: 5,
+                        fontStyle: 'italic', textAlign: 'left',}}>{expense} PKR</Text>
+                    </Pressable>
+                
+            </View>
         </View>
-          
-        </LinearGradient>  
+
       </View>
       <View>
       <FloatingAction actions={actions}
@@ -148,24 +131,24 @@ const Dashboard = () => {
         justifyContent: "center",
         position: "absolute",
         top: "50%",
-        left: "50%",
+        left: "40%",
         elevation: 5,
         transform: [{ translateX: -(width * 0.4) }, 
                     { translateY: -90 }],
-        height: 280,
-        width: width * 0.8,
-        backgroundColor: "#fff",
+        height: 480,
+        width: width * 1,
+        backgroundColor: "black",
         borderRadius: 10,}}>
-                        <TextInput placeholder="Enter value" placeholderTextColor='black'
-                                   value={inputValue} style={{width: "80%",
+                        <TextInput placeholder="Enter value" placeholderTextColor='white'
+                                   value={expense} style={{width: "80%",
         borderRadius: 5,
         paddingVertical: 8,
         paddingHorizontal: 16,
         borderColor: "rgba(0, 0, 0, 0.2)",
         borderWidth: 1,
         marginBottom: 8,
-        color: 'black'}} 
-                                   onChangeText={(value) => setInputValue(value)} />
+        backgroundColor: '#292D3E'}} 
+                                   onChangeText={(value) => setExpense(value)} />
                                    
       <DropDownPicker
         open={open}
@@ -175,7 +158,7 @@ const Dashboard = () => {
         setValue={setValue}
         setItems={setItems}
         maxHeight={200}
-        theme="LIGHT"
+        theme="DARK"
         multiple={false}
         searchable={true}
         ListMode={"SCROLLVIEW"}
@@ -185,21 +168,17 @@ const Dashboard = () => {
         paddingHorizontal: 16,
         borderColor: "rgba(0, 0, 0, 0.2)",
         borderWidth: 1, borderRadius: 5,
-        marginBottom: 8, alignSelf:'center'}}
+        marginBottom: 8, alignSelf:'center',}}
       />
+
+    <DatePicker textColor= {'white'} mode= {'date'} androidVariant={"nativeAndroid"} date={date} onDateChange={setDate} style={{ backgroundColor: 'transparent'}} />
     <View style={{flexDirection:'row', alignItems:'baseline'}}>
-    <Pressable title="Add" style={{height: 35, width: 60, backgroundColor: '#3178C6', alignSelf: 'center', marginTop: 80, marginRight: 80, borderRadius: 6 , }}>
+                            {/** This button is responsible to close the modal */}
+                        <Pressable title="Close" onPress={toggleModalVisibility} style={{height: 35, width: 60, backgroundColor: '#01DBB7', alignSelf: 'center', marginTop: 80, borderRadius: 6 , }}>
                         <Text style={{color: 'white' ,
                         fontSize: 20, 
                         marginTop: 5,
                         fontStyle: 'bold', alignSelf:'center'}}>Add</Text>
-                        </Pressable>
-                        {/** This button is responsible to close the modal */}
-                        <Pressable title="Close" onPress={toggleModalVisibility} style={{height: 35, width: 60, backgroundColor: '#3178C6', alignSelf: 'center', marginTop: 80, borderRadius: 6 , }}>
-                        <Text style={{color: 'white' ,
-                        fontSize: 20, 
-                        marginTop: 5,
-                        fontStyle: 'bold', alignSelf:'center'}}>Close</Text>
                         </Pressable>
                         </View>
                     </View>
--- screens/Login.js
+++ screens/Login.js
@@ -6,102 +6,89 @@
   const Login = () => {
     return (
       <KeyboardAwareScrollView
-      style={{ backgroundColor: '#4c69a5' }}
+      style={{ backgroundColor: 'black' }}
       resetScrollToCoords={{ x: 0, y: 0 }}
-      contentContainerStyle={styles.container}
       scrollEnabled={true}>
       <View style={{ height: 1000, width: 'auto'}}>
-      <LinearGradient start = {{x:0, y:0}} end={{x:1, y:1}} colors={['#1d0ccb', '#3f3dcb', '#af322c']} locations = {[0.03, 0.1, 0.88]}style = {{height: 1000, width: 'auto'}}>
-        <Image source={{uri: 'https://o.remove.bg/downloads/d4788af6-f573-4583-899c-6c51b8e0486a/construction-costs-abstract-concept_335657-3116-removebg-preview.png'}} style={{marginTop: 30, marginLeft: 30, width: 400, height: 400, resizeMode: 'cover',}}/>
+      <Image source={{uri: 'https://o.remove.bg/downloads/d4788af6-f573-4583-899c-6c51b8e0486a/construction-costs-abstract-concept_335657-3116-removebg-preview.png'}} style={{marginTop: 20, width: 300, height: 300, alignSelf: 'center',resizeMode: 'cover',}}/>
         <Text style={{color: "white",
             fontSize: 32,
             lineHeight: 84,
-            fontWeight: "Italics",
+            fontWeight: "Bold",
             textAlign: "center",
             marginBottom: 30,
+            alignSelf: 'center',
             }}>Personal Expense Tracker</Text>
 
-        <View style={{width: 'auto', height: 700, marginTop: 20, borderRadius: 30, backgroundColor: 'white' }}>
-        <Image source={{uri: 'https://o.remove.bg/downloads/62fd7d49-1cc2-45d5-961d-0b6d2aa5415d/image-removebg-preview.png'}} style={{height: 30, width: 150, marginLeft: 150, resizeMode: 'cover',}}></Image>   
+        <View style={{width: 'auto', height: 700, marginTop: 20, borderRadius: 30, backgroundColor: 'black' }}>
+        <Image source={{uri: 'https://o.remove.bg/downloads/62fd7d49-1cc2-45d5-961d-0b6d2aa5415d/image-removebg-preview.png'}} style={{height: 30, width: 150, alignSelf: 'center', resizeMode: 'cover',}}></Image>   
         <TextInput
-          placeholder="Enter name" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
+          placeholder="Enter name" placeholderTextColor='white'
+          style={{ backgroundColor: '#292D3E', 
           color: 'black' ,
           fontSize: 20, 
           fontStyle: 'bold', 
-          marginTop: 100, 
-          marginLeft: 30, 
+          marginTop: 70,alignSelf: 'center', 
           padding: 8, 
           width: 400,
           textAlign: 'center', 
-          paddingTop: 10, 
-          borderColor: '#28558a', 
+          paddingTop: 10,  
           borderWidth: 2, 
           borderRadius: 10, }}
         />
         <TextInput
-          placeholder="Enter email" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
+          placeholder="Enter email" placeholderTextColor='white'
+          style={{ backgroundColor: '#292D3E', 
           fontSize: 20, 
           fontStyle: 'bold', 
-          marginTop: 20, 
-          marginLeft: 30, 
+          marginTop: 20, alignSelf: 'center', 
           padding: 8, 
           width: 400,
           textAlign: 'center', 
-          paddingTop: 10, 
-          borderColor: '#28558a', 
+          paddingTop: 10,  
           borderWidth: 2, 
           borderRadius: 10, }}
         />
         <TextInput
-          placeholder="Enter Debit Card Number" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
+          placeholder="Enter Debit Card Number" placeholderTextColor='white'
+          style={{ backgroundColor: '#292D3E', 
           fontSize: 20, 
           fontStyle: 'bold', 
-          marginTop: 20, 
-          marginLeft: 30, 
+          marginTop: 20, alignSelf: 'center',
           padding: 8, 
           width: 400,
           textAlign: 'center', 
           paddingTop: 10, 
-          borderColor: '#28558a', 
           borderWidth: 2, 
           borderRadius: 10, }}
         />
         <TextInput
-          placeholder="Enter Card Expiry Date" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
+          placeholder="Enter Card Expiry Date" placeholderTextColor='white'
+          style={{ backgroundColor: '#292D3E',
           fontSize: 20, 
           fontStyle: 'bold', 
-          marginTop: 20, 
-          marginLeft: 30, 
+          marginTop: 20, alignSelf: 'center',
           padding: 8, 
           width: 400,
           textAlign: 'center', 
           paddingTop: 10, 
-          borderColor: '#28558a', 
           borderWidth: 2, 
           borderRadius: 10, }}
         />
         <TextInput
-          placeholder="Enter Card 3 Digit Code" placeholderTextColor='black'
-          style={{ backgroundColor: '#f1f1f1', 
+          placeholder="Enter CSV" placeholderTextColor='white'
+          style={{ backgroundColor: '#292D3E', 
           fontSize: 20, 
           fontStyle: 'bold', 
-          marginTop: 20, 
-          marginLeft: 30, 
+          marginTop: 20, alignSelf: 'center',
           padding: 8, 
           width: 400,
           textAlign: 'center', 
           paddingTop: 10, 
-          borderColor: '#28558a', 
           borderWidth: 2, 
           borderRadius: 10, }}
         />
         </View>
-          
-        </LinearGradient>  
       </View>
       </KeyboardAwareScrollView>
     )
